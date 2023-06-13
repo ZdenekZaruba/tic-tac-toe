bad75dc8c0795dd1cb76140314b9f6bd8fbbca85
ZdenekZaruba <56403536+ZdenekZaruba@users.noreply.github.com>
2023-06-13 16:48:20 +0200

    uprava textu

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <h1>Vítejte ve hře Piskvorky!</h1>
+  <h1>Vítejte ve hře Piškvorky!</h1>
   <div class="center">
     <a href="game_pvp.php">Hrát PVP</a>
     <a href="game_pve.php">Hrát PVE</a>
